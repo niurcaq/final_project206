b149417a7f90871b76c3f8417b145b28253d9c71
Niurca Quirarte <niurcaq@umich.edu>
2024-04-29 19:55:06 -0400

    added func to make code easier to read/cleaner

--- data.py
+++ data.py
@@ -2,56 +2,30 @@ import sqlite3
 from top_artists_web import artist_retrieval, artist_table
 from spotify_api import gather_spotify_artist_info, gather_spotify_songs, spotify_tables
 from itunes_api import gather_itunes_artist_info, gather_itunes_songs, itunes_tables
-# from spotify_api import func
+
+def drop_and_create(table_name, columns):
+    cur.execute(
+        f"DROP TABLE IF EXISTS {table_name}"
+    )
+    cur.execute(
+        f"CREATE TABLE IF NOT EXISTS {table_name} ({columns})"
+    )
 
 # set up conn
 conn = sqlite3.connect('music.db')
 # set up cur
 cur = conn.cursor()
-# drop any previous tables
-cur.execute(
-    "DROP TABLE IF EXISTS artists"
-)
-# create artists table
-cur.execute(
-    "CREATE TABLE IF NOT EXISTS artists (artist_id INTEGER PRIMARY KEY, name TEXT UNIQUE)"
-)
-# drop any previous tables
-cur.execute(
-    "DROP TABLE IF EXISTS spotify_artists"
-)
-# create spotify table
-cur.execute(
-    "CREATE TABLE IF NOT EXISTS spotify_artists (artist_id INTEGER PRIMARY KEY, spotify_id TEXT UNIQUE, genre TEXT, popularity INTEGER)"
-)
-# drop any previous tables
-cur.execute(
-    "DROP TABLE IF EXISTS spotify_songs"
-)
-# create spotify table
-cur.execute(
-    "CREATE TABLE IF NOT EXISTS spotify_songs (artist_id INTEGER PRIMARY KEY, name TEXT, length INTEGER)"
-)
-# drop any previous tables
-cur.execute(
-    "DROP TABLE IF EXISTS itunes_artists"
-)
-# create spotify table
-cur.execute(
-    "CREATE TABLE IF NOT EXISTS itunes_artists (artist_id INTEGER PRIMARY KEY, itunes_id INTEGER, genre TEXT)"
-)
-# drop any previous tables
-cur.execute(
-    "DROP TABLE IF EXISTS itunes_songs"
-)
-# create spotify table
-cur.execute(
-    "CREATE TABLE IF NOT EXISTS itunes_songs (artist_id INTEGER PRIMARY KEY, name TEXT, length INTEGER)"
-)
+# drop any previous tables + create new ones
+drop_and_create('artists', 'artist_id INTEGER PRIMARY KEY, name TEXT UNIQUE')
+drop_and_create('spotify_artists', 'artist_id INTEGER PRIMARY KEY, spotify_id TEXT UNIQUE, genre TEXT, popularity INTEGER')
+drop_and_create('spotify_songs', 'artist_id INTEGER PRIMARY KEY, name TEXT, length INTEGER')
+drop_and_create('itunes_artists', 'artist_id INTEGER PRIMARY KEY, itunes_id INTEGER, genre TEXT')
+drop_and_create('itunes_songs', 'artist_id INTEGER PRIMARY KEY, name TEXT, length INTEGER')
+
 # commit changes
 conn.commit()
 
-# artists, artists2, artists3 contains 100 artists from top billboard
+# artists contains 100 artists from top billboard
 artists = artist_retrieval()
 
 # gather data from apis
@@ -68,12 +42,3 @@ for i in range(0,4):
     itunes_tables(itunes_artists, True)
     itunes_tables(itunes_songs, False)
 
-
-# get tuples of data from api and save as a list
-# spotify_info = gather_spotify_artist_info(artists)
-
-# populate spotify_artists table by calling this func 4 times
-# for i in range(0,4):
-#     spotify_artists_table(spotify_info)
-
-# itunes_info = gather_itunes_artist_info(artists)
